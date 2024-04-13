858638931d6100e62320f95d6ddfeccc396933cd
Sam Foreman <saforem2@gmail.com>
2024-04-13 14:20:09 -0500

    Update `lua/plusings/{astrocore,astroui}.lua`

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -15,13 +15,13 @@ return {
       large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      diagnostics_mode = 2, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
-      virtual_text = true,
+      virtual_text = false,
       underline = true,
     },
     -- vim options can be configured here
@@ -70,6 +70,57 @@ return {
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        ["<S-W>"] = {
+            "<cmd>HopWord<CR>",
+            desc = "Hop Word",
+        },
+        ["<S-h>"] = {
+            "<cmd>HopChar2<CR>",
+            desc = "Hop Char 2"
+        },
+        ["<leader>b<"] = {
+            "<cmd>BufferMovePrevious<cr>",
+            desc="[BarBar] Buffer Move Previous"
+        },
+        ["<leader>b>"] = {
+            "<cmd>BufferMoveNext<cr>",
+            desc="[BarBar] Buffer Move Next"
+        },
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        ["\\"] = {
+            "<cmd>lua require('notify').dismiss()<CR>",
+            desc = "Dismiss Notification"
+        },
+        ["gD"] = {
+            vim.lsp.buf.declaration,
+        },
+        ["<leader>\\"] = {"<cmd>QuartoPreview<cr>", desc = "Quarto Preview"},
+        ["<leader>d"] = {[["_d]], desc = "Beginning of line"},
+        ["0"] = {"^", desc = "Beginning of line"},
+        ["<C-J>"] = {"J", desc = "Join line"},
+        ["X"] = {
+            [[:keeppatterns substitute/\s*\%#\s*/\r/e <bar> normal! ==<CR>]],
+            desc = "Split line"
+        },
+        ["<leader>bn"] = {"<cmd>tabnew<cr>", desc = "New tab"},
+        ["<leader>bD"] = {
+            function()
+                require("astronvim.utils.status").heirline.buffer_picker(
+                    function(bufnr)
+                        require("astronvim.utils.buffer").close(bufnr)
+                    end)
+            end,
+            desc = "Pick to close"
+        },
+        -- tables with the `name` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        ["<leader>b"] = {name = "Buffers"},
+        -- ["<leader>tr"] = {"<cmd>IronRepl<cr>", desc = "Iron REPL"},
+        -- ["<leader>t<space>"] = {"<cmd>IronFocus<cr>", desc = "Iron Focus"},
+        -- ["<leader>tH"] = {"<cmd>IronHide<cr>", desc = "Iron Hide"}
+        -- quick save
+        ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
       },
       t = {
         -- setting a mapping to false will disable it
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -15,7 +15,7 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        Normal = { bg = "#1c1c1c" },
       },
       astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
