543bb72ed24bec4c45c7f902c5893a7b8041fdfb
Sam Foreman <saforem2@gmail.com>
2024-06-15 16:54:04 -0500

    Add `lua/plugins/cyberdream.lua`

---
+++ lua/plugins/cyberdream.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("cyberdream").setup({
+                -- Enable transparent background
+                transparent = false,
+                -- Enable italics comments
+                italic_comments = false,
+
+                -- Replace all fillchars with ' ' for the ultimate clean look
+                hide_fillchars = false,
+
+                -- Modern borderless telescope theme
+                borderless_telescope = true,
+
+                -- Set terminal colors used in `:terminal`
+                terminal_colors = true,
+
+                theme = {
+                    variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+                    highlights = {
+                        -- Highlight groups to override, adding new groups is also possible
+                        -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+                        -- Example:
+                        -- Comment = { fg = "#696969", bg = "NONE", italic = true },
+                        Normal = { fg = "#cccccc" },
+
+                        -- Complete list can be found in `lua/cyberdream/theme.lua`
+                    },
+
+                    -- Override a highlight group entirely using the color palette
+                    overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+                        -- Example:
+                        return {
+                            Comment = { italic = true },
+                            -- ["@property"] = { fg = colors.blue, bold = true },
+                        }
+                    end,
+
+                    -- Override a color entirely
+                    colors = {
+                        -- For a list of colors see `lua/cyberdream/colours.lua`
+                        -- Example:
+                        bg = "#1c1c1c",
+                        -- green = "#00ff00",
+                        -- magenta = "#ff00ff",
+                    },
+                },
+            })
+        end,
+    },
+}
