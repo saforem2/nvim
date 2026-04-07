a1a2d7b804707a29ad1d420392edc45f6e6a34b1
Sam Foreman <saforem2@gmail.com>
2026-04-07 08:10:59 -0500

    chore: add `after/plugin/picker.lua`

--- after/plugin/picker.lua
+++ after/plugin/picker.lua
@@ -1,29 +1,36 @@
-require("picker").setup()
+require("fzf-lua").setup({
+  winopts = { preview = { default = "bat" } },
+})
 
 local map = vim.keymap.set
+local fzf = require("fzf-lua")
 
 -- Find
-map("n", "<leader>ff", "<cmd>Picker files<cr>", { desc = "Find Files" })
-map("n", "<leader>fb", "<cmd>Picker buffers<cr>", { desc = "Buffers" })
-map("n", "<leader>fh", "<cmd>Picker help_tags<cr>", { desc = "Help Tags" })
-map("n", "<leader>fr", "<cmd>Picker registers<cr>", { desc = "Registers" })
-map("n", "<leader>fj", "<cmd>Picker jumps<cr>", { desc = "Jumps" })
-map("n", "<leader>fm", "<cmd>Picker marks<cr>", { desc = "Marks" })
-map("n", "<leader>fk", "<cmd>Picker key-mappings<cr>", { desc = "Keymaps" })
-map("n", "<leader>fc", "<cmd>Picker colorscheme<cr>", { desc = "Colorschemes" })
+map("n", "<leader>ff", fzf.files, { desc = "Find Files" })
+map("n", "<leader>fb", fzf.buffers, { desc = "Buffers" })
+map("n", "<leader>fh", fzf.helptags, { desc = "Help Tags" })
+map("n", "<leader>fr", fzf.registers, { desc = "Registers" })
+map("n", "<leader>fj", fzf.jumps, { desc = "Jumps" })
+map("n", "<leader>fm", fzf.marks, { desc = "Marks" })
+map("n", "<leader>fk", fzf.keymaps, { desc = "Keymaps" })
+map("n", "<leader>fc", fzf.colorschemes, { desc = "Colorschemes" })
 
 -- Search
-map("n", "<leader>sg", "<cmd>Picker grep<cr>", { desc = "Grep" })
-map("n", "<leader>sl", "<cmd>Picker lines<cr>", { desc = "Buffer Lines" })
-map("n", "<leader>s:", "<cmd>Picker cmd_history<cr>", { desc = "Command History" })
-map("n", "<leader>s/", "<cmd>Picker search_history<cr>", { desc = "Search History" })
-map("n", "<leader>sh", "<cmd>Picker highlights<cr>", { desc = "Highlights" })
-map("n", "<leader>se", "<cmd>Picker emoji<cr>", { desc = "Emoji" })
+map("n", "<leader>sg", fzf.live_grep, { desc = "Live Grep" })
+map("n", "<leader>sw", fzf.grep_cword, { desc = "Grep Word" })
+map("n", "<leader>sl", fzf.blines, { desc = "Buffer Lines" })
+map("n", "<leader>s:", fzf.command_history, { desc = "Command History" })
+map("n", "<leader>s/", fzf.search_history, { desc = "Search History" })
+map("n", "<leader>sh", fzf.highlights, { desc = "Highlights" })
 
--- LSP (via picker)
-map("n", "<leader>cs", "<cmd>Picker lsp_document_symbols<cr>", { desc = "Document Symbols" })
-map("n", "<leader>cS", "<cmd>Picker lsp_workspace_symbols<cr>", { desc = "Workspace Symbols" })
-map("n", "<leader>cr", "<cmd>Picker lsp_references<cr>", { desc = "References" })
+-- LSP
+map("n", "<leader>cs", fzf.lsp_document_symbols, { desc = "Document Symbols" })
+map("n", "<leader>cS", fzf.lsp_workspace_symbols, { desc = "Workspace Symbols" })
+map("n", "<leader>cr", fzf.lsp_references, { desc = "References" })
+
+-- Commands
+map("n", "<leader>sc", fzf.commands, { desc = "Commands" })
+map("n", "<leader>sk", fzf.keymaps, { desc = "Keymaps" })
 
 -- Meta
-map("n", "<leader>fp", "<cmd>Picker sources<cr>", { desc = "All Pickers" })
+map("n", "<leader>fp", fzf.builtin, { desc = "All Pickers" })
